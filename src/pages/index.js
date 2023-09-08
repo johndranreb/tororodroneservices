d64e9271340a0415394a5ba4eccc076ad9688dfa
johndranreb <jhon.antero93@gmail.com>
2023-09-08 21:34:20 +0800

    edit index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -605,7 +605,7 @@ export default function Home() {
 		<h1 className="p-4 text-4xl font-semibold leadi text-center">What our customers are saying about us</h1>
 	</div>
 	<div className="container flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
-		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg lg:w-1/2">
 			<div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-50">
 				<p className="relative px-6 py-1 text-lg italic text-center text-gray-800">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 text-green-600">
@@ -624,7 +624,7 @@ export default function Home() {
 				<p className="text-sm uppercase">Real Estate Agent</p>
 			</div>
 		</div>
-		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg lg:w-1/2">
 			<div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-50">
 				<p className="relative px-6 py-1 text-lg italic text-center text-gray-800">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 text-green-600">
@@ -643,13 +643,13 @@ export default function Home() {
 				<p className="text-sm uppercase">Sports Event Organizer</p>
 			</div>
 		</div>
-		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg lg:w-1/2">
 			<div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-50">
 				<p className="relative px-6 py-1 text-lg italic text-center text-gray-800">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 text-green-600">
 						<path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
 						<path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-					</svg>The level of detail captured by Tororo Drone Services during our special events is exceptional.
+					</svg>Tororo Drone Services delivered stunning aerial footage for our real estate listings, impressing both clients and prospects.
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 text-green-600">
 						<path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
 						<path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
@@ -657,18 +657,18 @@ export default function Home() {
 				</p>
 			</div>
 			<div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-green-600 text-gray-50">
-				<img src="https://source.unsplash.com/50x50/?portrait?3" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 bg-gray-300" />
-				<p className="text-xl font-semibold leadi">David Myers</p>
-				<p className="text-sm uppercase">Event Planner</p>
+				<img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 bg-gray-300" />
+				<p className="text-xl font-semibold leadi">John Smith</p>
+				<p className="text-sm uppercase">Real Estate Agent</p>
 			</div>
 		</div>
-		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg lg:w-1/2">
 			<div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-50">
 				<p className="relative px-6 py-1 text-lg italic text-center text-gray-800">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 text-green-600">
 						<path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
 						<path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-					</svg>The breathtaking aerial views of our special event captured by their drones added a magical touch to the entire occasion!
+					</svg>Tororo Drone Services delivered stunning aerial footage for our real estate listings, impressing both clients and prospects.
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 text-green-600">
 						<path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
 						<path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
@@ -676,9 +676,9 @@ export default function Home() {
 				</p>
 			</div>
 			<div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-green-600 text-gray-50">
-				<img src="https://source.unsplash.com/50x50/?portrait?4" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 bg-gray-300" />
-				<p className="text-xl font-semibold leadi">Michael Rodriguez</p>
-				<p className="text-sm uppercase">Wedding Planner</p>
+				<img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 bg-gray-300" />
+				<p className="text-xl font-semibold leadi">John Smith</p>
+				<p className="text-sm uppercase">Real Estate Agent</p>
 			</div>
 		</div>
 	</div>
